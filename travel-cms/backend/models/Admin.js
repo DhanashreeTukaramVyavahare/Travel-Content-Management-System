5e93fbfb11e81d2fd0e30cf0c165d34a08f7c3ae
Dhanashree <dhanashreevyavahare31@gmail.com>
2024-11-23 13:09:41 +0530

    react

--- travel-cms/backend/models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Store hashed passwords for security
-});
-
-module.exports = mongoose.model('Admin', adminSchema);
